cfa8cda7f4c40f95aeeb425e69dda58b8616996d
unknown <muhamed.webdevelopment@gmail.com>
2021-03-31 00:01:48 +0200

    ex 02

--- ex02/src/App.js
+++ ex02/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Plant from './Plant';
-
 function App() {
   return (
     <div className="App">
@@ -8,5 +7,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
